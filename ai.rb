7b3e6e87dc7a28331e58405ea6ab1637516179a6
Angel Alonso <alonsofonseca.angel@gmail.com>
2015-09-14 05:57:35 +0200

    loading into array of objects

--- ai.rb
+++ ai.rb
@@ -9,26 +9,49 @@
 # Read the possible classes in them(one per ruby file, maybe?), and show them as possible parameters when someone asks for help (--help)
 # When the parameter is given, give way to that ruby code. "That ruby code" should handle errors
 
-@helplist = []
+# This will be the array of modules found
+@mod4uselist = []
 
+
+# This is the object that defines a module found
+class Mod4use
+#  attr_accessor :name,:methods
+
+  def initialize(name, methods)
+    @name = name
+    @methods = methods
+  end
+
+  def getname
+    return @name
+  end
+
+  def getmethods
+    return @methods
+  end
+end
+
+
+# This searches our list of module rb files to build the array of them
 def load_all
   # List of files starting with "mod_" and ending with ".rb" in the given directory
   basedir = '.'
   files = Dir.glob("#{basedir}/mod_*.rb")
-  files.each do |exm|
-    classarray = []
-    require exm
-    File.open(exm).each do |line|
+  files.each do |modu|
+    classname = ""
+    methodarray = []
+    require modu
+    File.open(modu).each do |line|
       if line["class"]
         classline_words = line.split 
-        classarray[0] = classline_words[1]
+        classname = classline_words[1]
       end
       if line["def"] && !line["initialize"]
         defline_words = line.split
-        classarray = classarray + [defline_words[1]]
+        methodarray << defline_words[1]
       end
     end
-    @helplist = @helplist + [classarray]
+    @mod4uselist << Mod4use.new(classname,methodarray)
   end
 end
 
@@ -36,19 +59,32 @@ def give_help
   puts "Syntax:" 
   puts "#{__FILE__} <mode> <params>"
   puts "\nmodes:"
-  @helplist.each do |entry|
-    puts ("  - " + entry[0])
-    print("      ")
-    entry[1..entry.size-2].each do |opt|
-      print (opt+"|")
+  @mod4uselist.each do |entry|
+    puts ("  - " + entry.getname)
+    entry.getmethods.each do |methd|
+      print ("     " + methd + "\n")
     end 
-    puts(entry[entry.size-1])
   end 
 end
 
+def execute(mod4use,method4use)
+  possible_mods = []
+  @mod4uselist.each do |entry|
+    if entry.getname.include? mod4use
+      possible_mods << entry.getname
+    end
+  end
+  
+  if possible_mods.length > 1
+    puts "which one?"
+    puts possible_mods
+  end
+end
+
+
 load_all()
-if @helplist.include? ARGV[0]
-  puts "found"
+if @mod4uselist.map(&:getname).join(' ').include? ARGV[0]
+  execute(ARGV[0],ARGV[1])
 else
   give_help()
 end
